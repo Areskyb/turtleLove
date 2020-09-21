51db08901d6e24e9d600c8f30d5aab1f8572ce0e
Aresky Berkane <areskyberkane@gmail.com>
2020-09-21 17:51:56 +0200

    initial git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { animated, useSpring, config } from "react-spring";
+import "./App.css";
+import Button from "./components/Button";
 
 function App() {
+  const animTitle = useSpring({
+    from: {
+      opacity: 0,
+      fontSize: 0,
+    },
+    to: {
+      opacity: 1,
+      fontSize: 100,
+      color: "#3d405b",
+      fontFamily: "Press Start 2P",
+    },
+    config: config.gentle,
+  });
+  const [times, setTimes] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <animated.h1 style={animTitle}>
+        Make the tutle fuck {times} times 🙃
+      </animated.h1>
+      <Button setTimes={setTimes} times={times} />
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { useSpring } from "react-spring";
+
+function Button({ setTimes, times }) {
+  const triggerTurtleSound = async () => {
+    if (disabled) return;
+    audio.play();
+    setDisabled(true);
+    await setTimeout(() => setDisabled(false), 1400);
+    setTimes((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    let sound = new Audio("http://peal.io/download/0kh9r");
+    setAudio(sound);
+    return () => {};
+  }, []);
+
+  const [audio, setAudio] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+  const buttonAnimation = useSpring({
+    from: { opacity: 1 },
+  });
+
+  const ButtonBackground = styled.button`
+    background: ${!disabled ? "#e07a5f" : "#C2BFB0"};
+    height: 10%;
+    width: 20%;
+    border-radius: 20px;
+    outline: none;
+    border: none;
+  `;
+  return (
+    <div>
+      <ButtonBackground onClick={() => triggerTurtleSound()}>
+        <ButtonText>🐢 👉👌 🐢</ButtonText>
+        {disabled ? (
+          <img src="https://i.makeagif.com/media/2-15-2017/4CwzYn.gif" />
+        ) : null}
+      </ButtonBackground>
+    </div>
+  );
+}
+export default Button;
+const ButtonText = styled.h2`
+  font-size: 40px;
+  color: #3d410z;
+`;
